4e553734f9ffa98426ce501c6ec68f02f2364ea9
shuchenliu <shuchen@Jarvis-mac.local>
2022-01-19 23:18:08 +0800

    v1

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import Utils.RemoteConnector;
+
+public class Main {
+    public static void main(String[] args) {
+        // find the article need to be posted
+        // convert the article into hexo format
+        // new article in blog
+        // send article to server
+        // rebuild the blog
+    }
+}
---
+++ src/main/java/Service/FileServices.java
@@ -0,0 +1,13 @@
+package Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileServices {
+    public List<String> findArticle(String name){
+        List<String> fullPaths = new ArrayList<>();
+
+        return fullPaths;
+    }
+
+}
